a92d2128d7b60a2b6de6e6eff9cf48e956baf9ba
Nikola Irinchev <irinchev@me.com>
2015-09-18 23:01:08 +0300

    Update the braintree library

--- BraintreeBindings/Braintree.linkwith.cs
+++ BraintreeBindings/Braintree.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("Braintree.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true, Frameworks = "SystemConfiguration Security AudioToolbox PassKit CoreLocation AVFoundation CoreMedia Foundation MessageUI MobileCoreServices UIKit", LinkerFlags = "-lc++ -ObjC")]
+[assembly: LinkWith ("Braintree.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true, Frameworks = "Accelerate SystemConfiguration Security AudioToolbox CoreLocation AVFoundation CoreMedia Foundation MessageUI MobileCoreServices UIKit", WeakFrameworks="PassKit", LinkerFlags = "-lc++ -ObjC")]
